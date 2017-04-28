c9d3bad21cc06c230978af4a0f7d55134efa87eb
Liang Yang <liang.yang1980@gmail.com>
2017-04-28 02:57:09 -0700

    the fourth check in

--- smartcab/smartcab/agent.py
+++ smartcab/smartcab/agent.py
@@ -191,14 +191,14 @@ def run():
     #   display      - set to False to disable the GUI if PyGame is enabled
     #   log_metrics  - set to True to log trial and simulation results to /logs
     #   optimized    - set to True to change the default log file name
-    sim = Simulator(env, update_delay= 0.01, log_metrics=True, display=False, optimized = True)
+    sim = Simulator(env, update_delay= 0.001, log_metrics=True, display=False, optimized = True)
     
     ##############
     # Run the simulator
     # Flags:
     #   tolerance  - epsilon tolerance before beginning testing, default is 0.05 
     #   n_test     - discrete number of testing trials to perform, default is 0
-    sim.run(n_test=10, tolerance = 0.001)
+    sim.run(n_test=10, tolerance = 0.0005)
 
 
 if __name__ == '__main__':
